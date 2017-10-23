646130ef761caed4da9387499c60e6fc439b1399
Torsten Schaub <torsten@cs.uni-potsdam.de>
2017-10-23 13:52:34 +0200

    changed call

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# SYSTEM="clingo"
+SYSTEM="clingcon"
+
 # ENCODINGS="action-MPP-3.lp goal-D-r.lp output-MPPD-3.lp -c horizon=$2"
-ENCODINGS="xapf/action-M.ilp   xapf/goal-m.ilp"
+# ENCODINGS="xapf/action-M.ilp   xapf/goal-m.ilp"
+# ENCODINGS="xapf/action-M-XY.ilp   xapf/goal-m-XY.ilp"
+ENCODINGS="xapf/action-M.iclp   xapf/goal-m.iclp"
 # VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
 
 for f in $1/*.lp ;
 do
-    clingo $ENCODINGS $f;
+    $SYSTEM $ENCODINGS $f;
 done
 
 # CALL="clingo1facts assign.lp $1 | clingo $ENCODINGS control.lp - $3 $VISUALIZER"
